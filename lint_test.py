4b795dfb529cc32a3d59b983cea7dd82d59492d2
Nikolay Lyubenov <nlyubenov@pirinsoft.bg>
2021-10-23 16:03:10 +0300

    add an extra row

--- lint_test.py
+++ lint_test.py
@@ -5,5 +5,7 @@ run = lint.Run(["factorial.py"], do_exit=False)
 score = run.linter.stats["global_note"]
 if score < THRESHOLD:
     print("Linter failed: Score < threshold value")
+    tototosoijsadf
     sys.exit(1)
 sys.exit(0)
+// start from here
