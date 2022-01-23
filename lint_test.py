09ef11ff5253a4694aab244c8a7f2b03883695e3
gitkonstantinakk <konstantin.k.konstantinov@outlook.com>
2022-01-23 22:31:51 +0200

    linter foxed

--- lint_test.py
+++ lint_test.py
@@ -2,7 +2,7 @@ import sys
 from pylint import lint
 THRESHOLD = 5
 run = lint.Run(["factorial.py"], do_exit=False)
-score = run.linter.stats["global_note"]
+score = run.linter.stats.global_note
 if score < THRESHOLD:
     print("Linter failed: Score < threshold value")
     sys.exit(1)
