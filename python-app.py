f7d2fe455a59cb2f7fa63fd90bde281d0c3c4425
gitkonstantinakk <konstantin.k.konstantinov@outlook.com>
2022-02-28 15:11:51 +0200

    fix blank lines

--- python-app.py
+++ python-app.py
@@ -10,9 +10,11 @@ def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
 
+
 @app.route('/feature_1')
 def feature_1():
     return '<h1>Feature 1 deployed!</h1>'
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
