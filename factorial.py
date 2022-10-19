80492cf34947d0cfec8fd0641450320f06ed4e5c
Kaloyan Valkov <kdvalkov@gmail.com>
2022-10-19 20:29:23 +0300

    update code

--- factorial.py
+++ factorial.py
@@ -14,7 +14,7 @@ def sum_factorial():
     for i in range(50):
         final_list.append(factorial(i))
     result = sum(final_list)
-    print("Final SUM = {}".format(result))
+    print("Final SUM is {}".format(result))
     return result
 
 
